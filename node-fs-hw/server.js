32b19e417ca0b34b8455ff0c4d7d8bf7e275de61
tohzen <melvin.l.mobley@gmail.com>
2021-05-17 17:21:47 -0400

    finished

--- node-fs-hw/server.js
+++ node-fs-hw/server.js
@@ -1,21 +1,31 @@
 const http = require("http");
 const fs = require("fs");
+function deleteAfter7Seconds(res) {
+    setTimeout(() => {
+        fs.rmdir("content", { recursive: true }, function (err) {
+            if (err) {
+                console.log("Something is wrong: ", err);
+                //res.end("Something is wrong: ", err);
+            } else {
+                console.log("Content folder deleted");
+                //res.end("Content folder deleted");
+            }
+        });
+    }, 7000);
+}
 http
 .createServer(function (request, response) {
-    if (request.url === "/") {
-        fs.readFile("index.html", function (error, data) {
-            if (error) {
-                response.end(error);
+    if (request.url === "/create-directory" && request.method === "POST") {
+        fs.mkdir("content", function (err) {
+            if (err) {
+                response.end("Sorry there is an error: ", err);
             } else {
-                console.log(11);
-                response.writeHead(200, { "content-Type": "text/html" });
-                response.write(data);
-                return response.end();
+                console.log("Content Folder created");
+                response.end("Content Folder created");
             }
         });
     }
-
-    if (request.url === "/create-a-file" && request.method === "POST") {
+    if (request.url === "/create-text" && request.method === "POST") {
         let body = "";
         request.on("data", function (data) {
             body += data.toString();
@@ -24,50 +34,35 @@ http
             let parsedBody = JSON.parse(body);
             fs.writeFile(parsedBody.fileName, parsedBody.message, function (err) {
                 if (err) {
-                    response.end(err);
+                    response.end("Sorry there is an error: ", err);
                 } else {
-                    response.end("File Created");
+                    console.log(`${parsedBody.fileName} created`);
+                    response.end(`${parsedBody.fileName} created`);
                 }
             });
         });
     }
-    if (request.url === "/update-a-file" && request.method === "POST") {
+    if (request.url === "/new-folder-and-file" && request.method === "POST") {
         let body = "";
         request.on("data", function (data) {
             body += data.toString();
         });
         request.on("end", function () {
             let parsedBody = JSON.parse(body);
-            fs.appendFile(
-                parsedBody.fileName,
-                `\n${parsedBody.message}`,
-                function (err) {
-                    if (err) {
-                        response.end(err);
-                    } else {
-                        response.end("Updated File");
-                    }
+            fs.writeFile("content/verbage.txt", "Hardcoded Text!", function (err) {
+                if (err) {
+                    response.end("Sorry there is an error: ", err);
+                } else {
+                    deleteAfter7Seconds(response);
+                    console.log("verbage.txt created");
+                    response.end("verbage.txt created");
+                    // console.log(`${parsedBody.fileName} created`);
+                    // response.end(`${parsedBody.fileName} created`);
                 }
-                );
             });
-        }
-        if (request.url === "/delete-a-file" && request.method === "POST") {
-            let body = "";
-            request.on("data", function (data) {
-                body += data.toString();
-            });
-            request.on("end", function () {
-                let parsedBody = JSON.parse(body);
-                fs.unlink(parsedBody.fileName, function (err) {
-                    if (err) return response.end(err);
-                    response.end(`${parsedBody.fileName} File Deleted`);
-                });
-            });
-        }
-    })
-    .listen(3000, function () {
-        console.log("Server Started!!!");
-    });
-
-
-    // I HATE THIS CODE GRADE CRAPppppP
+        });
+    }
+})
+.listen(3000, function () {
+    console.log(`Server started at port 3000`);
+});
